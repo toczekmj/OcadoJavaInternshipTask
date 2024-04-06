fd56acadf7370dba8bc2de19ad5ff10ea6d07997
toczekmj <toczekmj@gmail.com>
2024-04-06 16:47:15 +0200

    make code more readable, and remove some unnecessary comments

--- src/main/java/com/ocado/basket/BasketSplitter.java
+++ src/main/java/com/ocado/basket/BasketSplitter.java
@@ -7,49 +7,51 @@ public class BasketSplitter {
     public BasketSplitter(String absolutePathToConfigFile){
         this._productsConfig = JsonLoader.loadConfig(absolutePathToConfigFile);
     }
+
+    /**
+     * Splits the provided list of product names into optimized baskets based on delivery options.
+     * @param items A list of product names to be split.
+     * @return      A mapping of delivery options to lists of product names.
+     */
     public Map<String, List<String>> split(List<String> items){
-        //first attempts to load products
         List<Product> productsToSplit = loadProductsToSplit(items);
 
-        //now create hashmap that counts how many items has specific delivery option available
-        HashMap<String, Integer> deliveryNameByItemCount = divide(productsToSplit);
+        //create hashmap that counts how many items has specific delivery option available
+        HashMap<String, Integer> deliveryFrequencyMap = generateFrequencyMap(productsToSplit);
 
-        //get delivery option with the biggest available number of products, that is as well our biggest basket group at the time
-        String currentMaxDelivery = getHashMapMaxValueEntry(deliveryNameByItemCount);
+        String mostFrequentDelivery = mapMaxValueKey(deliveryFrequencyMap);
         Map<String, List<String>> outputBasket = new HashMap<>();
 
-        //as long as we have more products - perform further splitting.
-        //with sanitized and properly provided data there shouldn't exist a possibility for our currentMaxDelivery to return null,
-        //but we can check it here just in case
-        while(currentMaxDelivery != null && !productsToSplit.isEmpty()){
-            String finalCurrentMaxDelivery = currentMaxDelivery;
+        while(mostFrequentDelivery != null && !productsToSplit.isEmpty()){
+            String finalCurrentMaxDelivery = mostFrequentDelivery;
             List<String> products = new ArrayList<>();
 
-            //check each element if contains desired delivery option, if so add it to output list,
-            //but since this particular element 'disappears' from the list, we have to update our hashmap
-            //otherwise we'll get not necessarily true outputs later, therefore we won't have optimal basket
+            //check each element if contains desired delivery option, if so add it to output list, and update hashmap
             for(int i = productsToSplit.size()-1; i >= 0; i--){
                 Product currentProduct = productsToSplit.get(i);
-                if(currentProduct.getDeliveryOptions().contains(finalCurrentMaxDelivery)){
-                    for(String deliveryOption : currentProduct.getDeliveryOptions())
-                        deliveryNameByItemCount.replace(deliveryOption, deliveryNameByItemCount.get(deliveryOption)-1);
-                    products.add(currentProduct.getProductName());
-                    productsToSplit.remove(currentProduct);
-                }
+
+                if(!currentProduct.getDeliveryOptions().contains(finalCurrentMaxDelivery)) continue;
+
+                for(String deliveryOption : currentProduct.getDeliveryOptions())
+                    deliveryFrequencyMap.replace(deliveryOption, deliveryFrequencyMap.get(deliveryOption)-1);
+
+                products.add(currentProduct.getProductName());
+                productsToSplit.remove(currentProduct);
             }
 
             outputBasket.put(finalCurrentMaxDelivery, products);
-            deliveryNameByItemCount.remove(currentMaxDelivery);
-            currentMaxDelivery = getHashMapMaxValueEntry(deliveryNameByItemCount);
+            deliveryFrequencyMap.remove(mostFrequentDelivery);
+            mostFrequentDelivery = mapMaxValueKey(deliveryFrequencyMap);
         }
         return outputBasket;
     }
 
-    private String getHashMapMaxValueEntry(Map<String, Integer> hashMap){
-        return hashMap.keySet().stream().max(Comparator.comparing(hashMap::get)).orElse(null);
-    }
-
-    private HashMap<String, Integer> divide(List<Product> productsToSplit){
+    /**
+     * Counts how many items have each specific delivery option available.
+     * @param productsToSplit A list of products objects which is used to initialize our HashMap.
+     * @return                A mapping of delivery options to their occurrence count in the provided products list.
+     */
+    private HashMap<String, Integer> generateFrequencyMap(List<Product> productsToSplit){
         HashMap<String, Integer> deliveryMap = new HashMap<>();
         for(Product product : productsToSplit){
             List<String> deliveryOptions = product.getDeliveryOptions();
@@ -67,12 +69,21 @@ public class BasketSplitter {
         return deliveryMap;
     }
 
+    /**
+     * Changes list of Strings into a list of Product objects.
+     * @param items     A list of Strings with products names.
+     * @return          A list of Product objects.
+     */
     private List<Product> loadProductsToSplit(List<String> items) {
         List<Product> output = new ArrayList<>();
         for(Product product : _productsConfig)
             if(items.contains(product.getProductName()))
                 output.add(product);
         return output;
+    }
+
+    private String mapMaxValueKey(Map<String, Integer> hashMap){
+        return hashMap.keySet().stream().max(Comparator.comparing(hashMap::get)).orElse(null);
     }
 }
 
